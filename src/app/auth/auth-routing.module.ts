44ff0f846a58e69dea729d064c77c59ba1f96052
Fabio <fabioescobarardila@hotmail.com>
2024-02-25 20:43:56 -0500

    Routing completado

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'signup', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'signup' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
