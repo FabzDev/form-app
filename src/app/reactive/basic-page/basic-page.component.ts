468296706d9460d6d66a438b9edc184571457341
Fabio <fabioescobarardila@hotmail.com>
2024-02-27 13:46:31 -0500

    Optimizando manejo de errores en field Producto

--- src/app/reactive/basic-page/basic-page.component.ts
+++ src/app/reactive/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 const play5 = { name: 'PlayStation 5', price: 500, inStorage: 30 }
 
@@ -20,7 +20,35 @@ export class BasicPageComponent implements OnInit{
   ngOnInit(): void {
     this.myForm.reset( play5 )
   }
-;
+
+  isNotValid(field: string):boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.touched
+  }
+
+  errorMessage(field: string): string {
+    if (this.isNotValid(field)){
+      const errors =  this.myForm.controls[field].errors;
+      for(const err in errors){
+        switch ( err ){
+          case 'required': return 'El producto es requerido';
+          case 'minlength': return `El nombre del producto debe tener al menos ${errors['minlength'].requiredLength} carácteres`;
+        }
+      }
+    }
+    return ''
+    // if (this.isNotValid(field)){
+    //   const errors =  this.myForm.controls[field].errors || {};
+    //   for(const key of Object.keys(errors)){
+    //       switch ( key ){
+    //         case 'required': return 'El producto es requerido';
+    //         case 'minlength': return `El nombre del producto debe tener al menos ${errors['minlength'].requiredLength} carácteres`;
+    //       }
+    //     }
+    //   }
+    //   return '';
+    }
+
+
 
   onSave(): void {
     if(this.myForm.invalid) return
