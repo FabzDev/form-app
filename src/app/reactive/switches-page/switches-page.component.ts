652ea4159e15feae07556e5dce0d6c5132428a8c
Fabio <fabioescobarardila@hotmail.com>
2024-02-28 22:20:23 -0500

    Validaciones implementadas en SwitchesPage

--- src/app/reactive/switches-page/switches-page.component.ts
+++ src/app/reactive/switches-page/switches-page.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+  public switchesForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    notifications: [true, Validators.required],
+    terms: [false, Validators.requiredTrue]
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  isNotValid(field: string):boolean | null{
+    return this.switchesForm.controls[field].errors && this.switchesForm.controls[field].touched
+  }
+
+  onSave(): void{
+    if(this.switchesForm.invalid){
+      this.switchesForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.switchesForm.value);
+
+  }
 
 }
