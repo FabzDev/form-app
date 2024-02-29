90b22bf9ce242883e44a86afe0d2f37805f3b984
Fabio <fabioescobarardila@hotmail.com>
2024-02-28 23:25:18 -0500

    Implementando cantBeStrider Validation

--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,7 +11,7 @@ export class RegisterPageComponent {
   public registerForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
     email: ['', Validators.required],
-    user: ['', Validators.required],
+    user: ['', [Validators.required, cantBeStrider]],
     pass: ['', [Validators.required, Validators.minLength(6)]],
     pass2: ['', Validators.required],
   })
@@ -22,6 +23,7 @@ export class RegisterPageComponent {
   }
 
   onSave(): void{
+
     if(this.registerForm.invalid) return;
 
     console.log(this.registerForm.value);
